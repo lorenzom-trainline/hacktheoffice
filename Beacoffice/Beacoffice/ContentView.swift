4d485a54f0168d7f39a95b0f4eecffcd89e8b174
Lorenzo Masucci <lorenzo.masucci@thetrainline.com>
2022-03-30 12:17:09 +0100

    Prototype 1
    Our first attempt for the receiver didn't worked

--- Beacoffice/Beacoffice/ContentView.swift
+++ Beacoffice/Beacoffice/ContentView.swift
@@ -6,10 +6,66 @@
 //
 
 import SwiftUI
+import Combine
+import CoreLocation
+
+class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    var didChange = PassthroughSubject<Void, Never>()
+    var locationManager = CLLocationManager()
+    var lastDistance = CLProximity.unknown
+    
+    override init() {
+        super.init()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager, status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                
+                if CLLocationManager.isRangingAvailable() {
+                    // all ok
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "734fe6d8-d91d-4838-9ef1-8be0734524a5")!
+        let constraint = CLBeaconIdentityConstraint(uuid: uuid, major: 1, minor: 1)
+        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: "MyBeacon")
+
+
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(satisfying: constraint)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity)
+        } else {
+            updateDistance(.unknown)
+        }
+    }
+
+    func updateDistance(_ distance: CLProximity) {
+        lastDistance = distance
+        print(distance)
+        didChange.send(())
+    }
+}
 
 struct ContentView: View {
+    @ObservedObject var detector = BeaconDetector()
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("\(detector.lastDistance.rawValue)")
             .padding()
     }
 }
