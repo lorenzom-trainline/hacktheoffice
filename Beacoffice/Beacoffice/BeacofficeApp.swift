0af20a863fde7cb6fdef45f72eb3024ef05a3362
Lorenzo Masucci <lorenzo.masucci@thetrainline.com>
2022-03-30 11:36:48 +0100

    New Project

---
+++ Beacoffice/Beacoffice/BeacofficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeacofficeApp.swift
+//  Beacoffice
+//
+//  Created by Lorenzo Masucci on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BeacofficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
