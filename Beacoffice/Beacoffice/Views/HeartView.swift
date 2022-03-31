e3b22e58b67ed6a3dfc7d3f497199ae3405d7a38
Lorenzo Masucci <lorenzo.masucci@thetrainline.com>
2022-03-31 16:02:24 +0100

    Add HeartView

---
+++ Beacoffice/Beacoffice/Views/HeartView.swift
@@ -0,0 +1,36 @@
+//
+//  HeartView.swift
+//  Beacoffice
+//
+//  Created by Lorenzo Masucci on 31/03/2022.
+//
+
+import SwiftUI
+
+struct HeartView: View {
+    @State private var animationAmount: CGFloat = 1
+    
+    var body: some View {
+        
+        Image(systemName: "heart.fill")
+            .resizable()
+            .frame(width: 50, height: 50)
+            .foregroundColor(.red)
+            .scaleEffect(animationAmount)
+            .animation(
+                .linear(duration: 0.6)
+                    .delay(0.1)
+                    .repeatForever(autoreverses: true),
+                value: animationAmount)
+            .onAppear {
+                animationAmount = 1.2
+            }
+        
+    }
+}
+
+struct HeartView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeartView()
+    }
+}
