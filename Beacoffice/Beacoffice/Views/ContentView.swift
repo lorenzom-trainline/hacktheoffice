2558d9b1cdcae715c9b7849401805af4c8449ba1
Lorenzo Masucci <lorenzo.masucci@thetrainline.com>
2022-03-31 15:02:15 +0100

    Fixing UI Updating

---
+++ Beacoffice/Beacoffice/Views/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Beacoffice
+//
+//  Created by Lorenzo Masucci on 30/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var beaconDetector = BeaconDetector()
+
+    var body: some View {
+        VStack {
+
+            Button(action: {
+                print("Button pressed")
+            }) {
+                HStack {
+                    Image(systemName: "bookmark.fill")
+                    Text("Detector")
+                }
+            }
+            .buttonStyle(GradientButtonStyle())
+            
+            VStack {
+                DetectorView(beaconDetector: beaconDetector)
+            }
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct GradientButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .foregroundColor(Color.white)
+            .padding()
+            .background(Color.green)
+//            .background(configuration.isPressed ? Color.green : Color.yellow)
+//            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(15.0)
+            .scaleEffect(configuration.isPressed ? 1.3 : 1.0)
+    }
+}
